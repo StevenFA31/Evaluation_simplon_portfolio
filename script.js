fe26382316880006dcd66f26442e4ad4404ed443
StevenFA31 <stevenferreiraalves@gmail.com>
2022-12-07 22:37:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll(".project");
+const overlay = document.querySelector(".overlay");
+const overlayImage = document.querySelector(".overlay__inner img");
+
+// function open(e) {
+//   overlay.classList.add("open");
+//   const src = e.currentTarget.querySelector("img").src;
+//   overlayImage.src = src;
+// }
+
+function close() {
+  overlay.classList.remove("open");
+}
+
+buttons.forEach((button) => button.addEventListener("click", open));
+overlay.addEventListener("click", close);
+
+document.querySelector("#contact-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = "";
+  e.target.elements.email.value = "";
+  e.target.elements.message.value = "";
+});
